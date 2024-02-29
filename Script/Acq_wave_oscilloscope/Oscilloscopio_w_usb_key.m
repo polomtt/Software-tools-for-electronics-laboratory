e405be5a2407b117bb75c7911f889aa03559c611
polomtt <72733574+polomtt@users.noreply.github.com>
2024-02-29 16:19:46 +0100

    Update usb key

--- Script/Acq_wave_oscilloscope/Oscilloscopio_w_usb_key.m
+++ Script/Acq_wave_oscilloscope/Oscilloscopio_w_usb_key.m
@@ -1,11 +1,12 @@
+filename = 'E:/PIXEL_D_A1_25_45_CENTRAL_'
+num_wave = 10
 
 DPO3000.IP = '10.196.31.127' ; % to check and modify each time
 DPO3000.P = tcpip(DPO3000.IP,4000); %create TCPIP object
 fopen(DPO3000.P); %connect TCPIP object
 
-
-for i=1:20
-    formatSpec = strcat('SAVE:WAVEFORM ALL,','"','E:/PIXEL_D_A1_25_45_CENTRAL_%d.csv','"');
+for i=1:num_wave
+    formatSpec = strcat('SAVE:WAVEFORM ALL,','"',filename,"_%d.csv",'"');
     str = sprintf(formatSpec,i);
     disp(str);
     fprintf(DPO3000.P,str);  
